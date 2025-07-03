11ed0f408e2cb75da539023c927f1f8e75e5a904
Manish <manishlodhi0224@gmail.com>
2025-07-03 12:41:20 +0530

    vite react project

---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){   // this is the component that we are making ... and name should start with capital letter
+    return(
+        <h2>this is test code </h2>
+    )
+}
+
+export default Chai

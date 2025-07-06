4eb731ae3bee9438508e33906d77b44e51c793b5
Manish <manishlodhi0224@gmail.com>
2025-07-06 22:32:06 +0530

    useEffect

---
+++ 07_useEffect/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect, useRef } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  // variation : 1 ... every render
+  // useEffect(()=>{
+  //   alert("i will run on every render")
+  // })
+
+  // variation : 2 ... first render only
+  // useEffect(()=>{
+  //   alert("i will run on firsts render only")
+  // },[])
+
+  // variation : 3 ... every time some variable is updated
+  // useEffect(()=>{
+  //   alert("count was clicked, hence i ran")     // this will run as soon as the browser is loaded(even before clicking 
+  //                         //  the button ) bcoz, count is initialized with 0 and that too is counted as change hence useEffect ran)
+
+  // },[count])
+
+  // variation : 4 ... cleanup fxn
+  // useEffect(()=>{
+  //   console.log("count changed")
+  //   return()=>{
+  //     console.log("element unmounted ")
+  //   }
+
+  // },[count]) 
+
+  const btn = useRef()
+
+  console.log(btn.current);
+  
+  return (
+    <>
+
+      <div className="card">
+        {/* <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button> */}
+
+        <button ref={btn}
+          onClick={() => {
+            setCount((count) =>  {
+              console.log("count is being clicked");
+              return count+1} )        // explicit return concept
+          }}>click me {count}</button>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App

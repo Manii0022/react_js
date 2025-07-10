e39eda1feadc1bd30f5be77bfde68d0349d951cf
Manish <manishlodhi0224@gmail.com>
2025-07-10 14:39:13 +0530

    react router website

--- 09_React_Router/src/components/Github/Github.jsx
+++ 09_React_Router/src/components/Github/Github.jsx
@@ -1,14 +1,35 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+function Github() {
 
-class Github extends Component {
-    render() {
-        return (
-            <div className='text-center m-4 bg-gray-600 text-white'>
-                Github Followers  : 
-                
-            </div>
-        );
-    }
+    const data = useLoaderData()
+
+    // const [data , setData] = useState([])
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/Manii0022')
+    //     .then((response)=> response.json())
+    //     .then(data =>{
+    //         console.log(data);
+    //         setData(data)
+
+    //     })
+    // },[])
+
+
+    return (
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+            Github Followers : {data.followers}
+            <img src={data.avatar_url} alt='github image' width={300} />
+        </div>
+    );
+
 }
 
+
 export default Github;
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/Manii0022')
+    return response.json()
+
+}

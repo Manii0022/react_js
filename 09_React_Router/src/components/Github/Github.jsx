2f8fde1a41fbdcd75efdd1f3f6f3d7245f65cf6b
Manish <manishlodhi0224@gmail.com>
2025-07-10 13:52:56 +0530

    react router website

---
+++ 09_React_Router/src/components/Github/Github.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Github extends Component {
+    render() {
+        return (
+            <div className='text-center m-4 bg-gray-600 text-white'>
+                Github Followers  : 
+                
+            </div>
+        );
+    }
+}
+
+export default Github;
--- 09_React_Router/src/main.jsx
+++ 09_React_Router/src/main.jsx
@@ -2,9 +2,50 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path : "/",      // this is the main path 
+//     element: <Layout/>,
+//     children:[     // yha / k baad aane waale components ayenge ie. children 
+//       {
+//         path : "",       // this is simply /
+//         element: <Home/>
+//       },
+//       {
+//         path : "about",       // // this is  /about
+//         element : <About/>
+//       },
+//       {
+//         path : "contact",     // this is  /contact
+//         element : <Contact/>
+//       }
+//     ]
+//   }
+// ])
+
+// another way of creating router..more readable way 
+const router = createBrowserRouter(
+  createRoutesFromElements(   // sbse pehla parent path,, fir nesting hoti ja rhi hai 
+    <Route path = "/" element={<Layout/>} >
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>  
+      <Route path='contact' element={<Contact/>}/>
+      <Route path='user/:userId' element={<User/>}/>
+      <Route path='user/:userId' element={<User/>}/>
+
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )

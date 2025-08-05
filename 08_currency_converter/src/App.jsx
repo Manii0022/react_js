e6a27a804276d5a640824aeaada5e81686427c9a
Manish <manishlodhi0224@gmail.com>
2025-08-05 22:36:41 +0530

    changes

--- 08_currency_converter/src/App.jsx
+++ 08_currency_converter/src/App.jsx
@@ -60,7 +60,6 @@ function App() {
                                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 
                                 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
                                 onClick={swap}
-                                
                             >
                                 swap
                             </button>
